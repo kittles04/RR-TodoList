c95ef9b6968be7fa30f25c7cf709d0b8bd747261
Kerri Walker <kerriannwalker3@gmail.com>
2020-05-10 10:23:55 -0400

    Change id to index in parameter of TOdoList so I cancross out in the UI

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Todo from "./Todo";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Todo from './Todo';
 
 const TodoList = ({ todos, onTodoClick }) => (
   <ul>
     {todos.map((todo, index) => {
       console.log(todos);
       return (
-        // <Todo key={todo.id} {...todo} onClick={() => onTodoClick(0)} /> This works!
-        // index, the second parameter of the map function, will automatically give you the item index
-        <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+        <Todo key={todo.index} {...todo} onClick={() => onTodoClick(index)} />
       );
     })}
   </ul>
@@ -20,10 +18,10 @@ TodoList.propTypes = {
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       completed: PropTypes.bool.isRequired,
-      text: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired
     }).isRequired
   ).isRequired,
-  onTodoClick: PropTypes.func.isRequired,
+  onTodoClick: PropTypes.func.isRequired
 };
 
 export default TodoList;
